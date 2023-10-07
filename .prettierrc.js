946539c22ba78e40efc805dac35322eb7bd2a591
Ibrahim Saberi <georgeipsum@gmail.com>
2023-10-07 14:25:03 -0400

    add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 80,
+  arrowParens: "always",
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSameLine: false,
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "^react$",
+    "^server$",
+    "<THIRD_PARTY_MODULES>",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators"],
+  endOfLine: "lf",
+  tailwindConfig: "./tailwind.config.ts",
+};
