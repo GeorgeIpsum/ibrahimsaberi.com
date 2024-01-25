b6e7d614dd4ae46628b5e61f53cc7f945c04e14c
Ibrahim Saberi <georgeipsum@gmail.com>
2024-01-25 15:49:31 -0500

    recreate babymode expressjs (NO REGEX) because.................. just because

---
+++ 2d/curl.js
@@ -0,0 +1,69 @@
+const { Curl } = require("node-libcurl");
+
+const handleCurlCommand = async ({ ua, libcurlC }) => {
+  if (!ua) throw new Error("user agent not defined");
+  if (!libcurlC) throw new Error("curl command not");
+
+  const promise = new Promise((resolve, reject) => {
+    const curl = new Curl();
+    curl.setOpt("USERAGENT", ua);
+
+    const cToLines = libcurlC.split("\n");
+    cToLines.forEach((line) => {
+      const isSetOpt = line.includes("curl_easy_setopt(hnd,");
+      if (isSetOpt) {
+        const [_, rawopt, rawval] = new RegExp(
+          /curl_easy_setopt\(hnd,.?(\S*),.?(\S*)\);/
+        ).exec(line);
+        const opt = rawopt.split("CURLOPT_")[1];
+        if (opt in Curl.option) {
+          let val = rawval;
+          if (
+            // eslint-disable-next-line
+            val.indexOf('\"') === 0 &&
+            // eslint-disable-next-line
+            val.lastIndexOf('\"') === val.length - 1
+          ) {
+            val = val.slice(1, val.length - 1);
+          }
+          curl.setOpt(Curl.option[opt], val);
+        } else {
+          console.log("unsupported curl opt", rawopt, val);
+        }
+      }
+    });
+    curl.on("end", (status, data) => {
+      resolve({
+        status,
+        data,
+      });
+    });
+    curl.on("error", (error, code) => {
+      reject({
+        error,
+        code,
+      });
+    });
+    curl.perform();
+  });
+
+  const res = await promise;
+
+  return res;
+};
+
+const handler = async (req, res) => {
+  const body = req.data;
+  const json = JSON.parse(body);
+  const curlResult = await handleCurlCommand(json).catch((e) => ({
+    error: e.error,
+    code: e.code,
+  }));
+  if (curlResult.status !== 200) {
+    console.log("something went wrong");
+  }
+  res.writeHead(200, "STUFF = CURLED");
+  res.end(curlResult.data);
+};
+
+module.exports = { handleCurlCommand, handler };
--- 2d/index.js
+++ 2d/index.js
@@ -1,81 +1,65 @@
 const { Server } = require("node:http");
-const { Curl } = require("node-libcurl");
+const { createHmac } = require("node:crypto");
+const curl = require("./curl");
 
-const handleCurlCommand = async ({ ua, libcurlC }) => {
-  if (!ua) throw new Error("user agent not defined");
-  if (!libcurlC) throw new Error("curl command not");
-
-  const promise = new Promise((resolve, reject) => {
-    const curl = new Curl();
-    curl.setOpt("USERAGENT", ua);
-
-    const cToLines = libcurlC.split("\n");
-    cToLines.forEach((line) => {
-      const isSetOpt = line.includes("curl_easy_setopt(hnd,");
-      if (isSetOpt) {
-        const [_, rawopt, rawval] = new RegExp(
-          /curl_easy_setopt\(hnd,.?(\S*),.?(\S*)\);/
-        ).exec(line);
-        const opt = rawopt.split("CURLOPT_")[1];
-        if (opt in Curl.option) {
-          let val = rawval;
-          if (
-            // eslint-disable-next-line
-            val.indexOf('\"') === 0 &&
-            // eslint-disable-next-line
-            val.lastIndexOf('\"') === val.length - 1
-          ) {
-            val = val.slice(1, val.length - 1);
-          }
-          curl.setOpt(Curl.option[opt], val);
-        } else {
-          console.log("unsupported curl opt", rawopt, val);
-        }
-      }
-    });
-    curl.on("end", (status, data) => {
-      resolve({
-        status,
-        data,
-      });
-    });
-    curl.on("error", (error, code) => {
-      reject({
-        error,
-        code,
-      });
-    });
-    curl.perform();
-  });
-
-  const res = await promise;
-
-  return res;
+const authPasses = {};
+const createAuthPass = (url) => {
+  const key = createHmac("sha256", JSON.stringify({ url }));
+  authPasses[key] = {};
 };
 
-const server = new Server(async (req, res) => {
-  console.log("Received request:", req.method, req.url);
-  if (req.method === "POST" && req.url.split("/")[1] === "curl") {
+const emptyHandler = async (req, res) => {
+  res.writeHead(404);
+  res.end("B)");
+};
+
+const routes = [{ url: "", methods: [], func: emptyHandler }];
+const reqMatcher = (req) => [req.method, req.url.split("/")[1]];
+function getFunc(method, url) {
+  const route = routes.find((route) => route.url === url);
+  if (!route) {
+    console.log("NO ROUTE FOUND FOR ROUTE", url);
+    return emptyHandler;
+  }
+  return route.methods.find((m) => m.method === method)?.func ?? route.func;
+}
+function register(url, method, func) {
+  const exists = routes.find((route) => route.url === url);
+  if (!exists) {
+    routes.push({ url, methods: [{ method, func }], func });
+  } else {
+    exists.methods.push({ method, func });
+    exists.func = func;
+  }
+}
+async function setReqData(req) {
+  return new Promise((resolve) => {
     let requestData = "";
     req.on("data", (datum) => {
       requestData += datum;
     });
     req.on("close", async () => {
       const body = requestData.toString("utf-8");
-      const json = JSON.parse(body);
-      const curlResult = await handleCurlCommand(json).catch((e) => ({
-        error: e.error,
-        code: e.code,
-      }));
-      if (curlResult.status !== 200) {
-        console.log("something went wrong");
-      }
-      res.writeHead(200, "STUFF = CURLED");
-      res.end(curlResult.data);
+      req.data = body;
+      resolve();
     });
-  } else {
-    res.writeHead(200);
-    return res.end(`${req.method}\n${req.url}\n\nB) whattup puddin cup\n`);
+  });
+}
+async function run(req, res) {
+  const [method, url] = reqMatcher(req);
+  const func = getFunc(method, url);
+  await setReqData(req);
+  return await func(req, res);
+}
+
+register("curl", "POST", curl.handler);
+const server = new Server(async (req, res) => {
+  console.log("Received request:", req.method, req.url);
+  try {
+    await run(req, res);
+  } catch (e) {
+    console.log(e);
+    console.log("FAILED");
   }
 });
 
