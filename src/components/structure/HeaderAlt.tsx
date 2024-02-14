18280ebc67e1ca588ca18f81b4a19e613102fd3f
Ibrahim Saberi <georgeipsum@gmail.com>
2024-02-14 16:08:42 -0500

    settings gear

--- src/components/structure/HeaderAlt.tsx
+++ src/components/structure/HeaderAlt.tsx
@@ -4,7 +4,7 @@ import ThemeToggleSsr from "@/features/theme/ThemeToggleSsr";
 
 import { Container } from "../atoms";
 import { Logo } from "../navigation";
-import { Gear } from "../svg";
+import SettingsGear from "../singletons/SettingsGear";
 
 const headerLinks = [
   {
@@ -46,7 +46,7 @@ const HeaderAlt: React.FC = () => {
         <div className="flex items-center justify-center p-1 sm:hidden">
           <SettingsGear />
         </div>
-        <Container className="hidden w-fit items-center gap-2 rounded-xl sm:flex">
+        <Container className="relative hidden w-fit items-center gap-2 rounded-xl sm:flex">
           <SettingsGear />
           <div className="flex h-full items-center">
             <ThemeToggleSsr />
@@ -54,20 +54,6 @@ const HeaderAlt: React.FC = () => {
         </Container>
       </div>
     </header>
-  );
-};
-
-const SettingsGear: React.FC = () => {
-  return (
-    <button className="group grid h-full place-content-center place-items-center  p-[2px] transition-all duration-150">
-      <div className="-z-10 col-start-1 col-end-1 row-start-1 row-end-1 h-7 w-7 rounded-full bg-pink-300 bg-gradient-to-br from-rose-600/30 to-pink-200/30 transition-all duration-150 group-hover:bg-pink-200  group-hover:opacity-0 dark:bg-emerald-700 dark:from-teal-500/50 dark:to-emerald-800/50 group-hover:dark:bg-emerald-800" />
-      <div className="-z-20 col-start-1 col-end-1 row-start-1 row-end-1 h-7 w-7 rounded-full bg-pink-200  transition-all duration-150 dark:bg-emerald-800" />
-      <Gear
-        height={20}
-        width={20}
-        className="col-start-1 col-end-1 row-start-1 row-end-1 fill-rose-50/90 transition-all duration-500 group-hover:rotate-180 group-hover:fill-white dark:fill-emerald-100/80 "
-      />
-    </button>
   );
 };
 
