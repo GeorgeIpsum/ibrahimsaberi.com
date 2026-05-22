bed0554c5b4a1dbce702e6412348287926002b85
Ibrahim Saberi <georgeipsum@gmail.com>
2026-05-22 03:17:48 -0400

    chore: content updates

---
+++ src/services/basin/components/review-badge.tsx
@@ -0,0 +1,16 @@
+import { useCallback, useMemo } from "react";
+
+export const ReviewTag: React.FC<{ tag: string; otherTags: string[] }> = ({
+  tag,
+  otherTags,
+}) => {
+  const verdict = useMemo(() => {
+    const verdict = otherTags.find((t) => t.startsWith("verdict-"));
+    if (!verdict) return null;
+    return verdict.replace("verdict-", "");
+  }, [otherTags]);
+
+  const IconComponent = useCallback(() => {
+    const reviewType = tag.replace("-review", "");
+  }, [tag]);
+};
