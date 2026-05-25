7bd309b7f84cd508f141e6c11a4f08b33616d54a
Ibrahim Saberi <georgeipsum@gmail.com>
2026-05-25 14:50:58 -0400

    chore: env update

--- src/env.ts
+++ src/env.ts
@@ -9,6 +9,7 @@ export const env = createEnv({
     REDIS_KV_REST_API_TOKEN: type("string > 0"),
     REDIS_KV_REST_API_URL: type("string > 0"),
     OPENWEATHERMAP_API_KEY: type("string | undefined"),
+    LE_PLATFORM_API_KEY: type("string | undefined"),
   },
   client: {},
   runtimeEnv: {
@@ -18,6 +19,7 @@ export const env = createEnv({
     REDIS_KV_REST_API_TOKEN: process.env.REDIS_KV_REST_API_TOKEN,
     REDIS_KV_REST_API_URL: process.env.REDIS_KV_REST_API_URL,
     OPENWEATHERMAP_API_KEY: process.env.OPENWEATHERMAP_API_KEY,
+    LE_PLATFORM_API_KEY: process.env.LE_PLATFORM_API_KEY,
   },
   emptyStringAsUndefined: true,
 });
