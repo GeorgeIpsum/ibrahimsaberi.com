bd27d7324704ff49ae033dd36b8f2270ec64a39a
Ibrahim Saberi <georgeipsum@gmail.com>
2026-06-21 15:50:27 -0400

    refactor: remove noise texture from reflection page, it doesnt add much

--- src/app/reflection/page.tsx
+++ src/app/reflection/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import { LightRays, NoiseTexture } from "@/components/backgrounds";
+import { LightRays } from "@/components/backgrounds";
 import { Reflect_ } from "../../services/reflection/components/reflect";
 
 export default async function Page({
@@ -9,9 +9,9 @@ export default async function Page({
 }) {
   return (
     <article className="flex h-dvh w-screen items-center justify-center bg-black">
-      <LightRays className="isolate" color="rgba(70, 25, 1, 0.67)" />
-      <div className="absolute inset-0 animate-pulse shadow-amber-100 [box-shadow:inset_0_0_30px_1px_var(--color-amber-950)]">
-        <NoiseTexture frequency={0.7} noiseOpacity={0.2} />
+      <LightRays className="isolate" color="rgba(70, 25, 1, 1)" />
+      <div className="absolute inset-0 animate-pulse shadow-amber-50 [box-shadow:inset_0_0_30px_1px_var(--color-amber-950)]">
+        {/* <NoiseTexture frequency={0.7} noiseOpacity={0.2} /> */}
       </div>
       <div className="isolate">
         <Reflect_ searchParams={searchParams} />
