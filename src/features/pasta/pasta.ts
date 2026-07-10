0b2d3e2452c25a56d237bbbc1adddbcac1ea1893
Ibrahim Saberi <georgeipsum@gmail.com>
2026-07-09 20:43:19 -0400

    feat: include all known ascii art in pasta

--- src/features/pasta/pasta.ts
+++ src/features/pasta/pasta.ts
@@ -1,13 +1,20 @@
 import { readdirSync, readFileSync } from "node:fs";
 import path from "node:path";
+import * as ASCII_PASTA from "@/utils/ascii";
 
 const copypastaDir = path.join(process.cwd(), "src/features/pasta/sauce");
 
-export const copypasta = readdirSync(copypastaDir)
-  .filter((file) => file.endsWith(".txt"))
-  .map((file) => ({
-    title: file,
-    content: readFileSync(path.join(copypastaDir, file), "utf8"),
-  }));
+export const copypasta = [
+  ...readdirSync(copypastaDir)
+    .filter((file) => file.endsWith(".txt"))
+    .map((file) => ({
+      title: file,
+      content: readFileSync(path.join(copypastaDir, file), "utf8"),
+    })),
+  ...Object.entries(ASCII_PASTA).map(([title, content]) => ({
+    title: `${title}.txt`,
+    content,
+  })),
+];
 
 export const allSauce = copypasta.map((p) => p.title.replace(".txt", ""));
