ccb99654e407520c689c9187963b014bd4a1ddf6
Ibrahim Saberi <georgeipsum@gmail.com>
2026-07-13 15:44:14 -0400

    chore: browser images

--- src/app/(root)/tots/{lib}/groups/supporting.tsx
+++ src/app/(root)/tots/{lib}/groups/supporting.tsx
@@ -1,3 +1,8 @@
+import arcImage from "@public/img/pages/tots/arc.svg";
+import nookImage from "@public/img/pages/tots/nook.png";
+import oraImage from "@public/img/pages/tots/ora.png";
+import shiftImage from "@public/img/pages/tots/shift.png";
+import zenImage from "@public/img/pages/tots/zen.svg";
 import { MessageCircleHeart } from "lucide-react";
 import type { ToolGroup } from "../types";
 
@@ -8,12 +13,14 @@ export const supportingTools = {
   tools: [
     {
       name: "zen browser",
+      image: zenImage,
       platform: "nixos",
       description:
         "if there wasnt already a flake for this id honestly just use firefox.",
     },
     {
       name: "arc",
+      image: arcImage,
       description:
         "it does the job fairly well, but i don't really want to continue supporting $company in any capacity (not to mention the random/ HUGE security issues that have popped up in the past). at least it's not (entirely) chrome.",
       lookingForReplacement: true,
@@ -21,20 +28,24 @@ export const supportingTools = {
       evaluating: [
         {
           name: "nook",
+          image: nookImage,
           description:
             "still kind of early, but promising. why are there so many arc clones.",
         },
         {
           name: "ora",
+          image: oraImage,
           description: "too alpha, and unfortunately webkit based",
         },
         {
           name: "zen",
+          image: zenImage,
           description:
-            "please firefox i'm begging you to stop sucking on macos",
+            "please firefox/gecko i'm begging you to stop sucking on macos",
         },
         {
           name: "shift",
+          image: shiftImage,
           description:
             'interesting but is by far the most "product"-ey (derogatory) out of all of these.',
         },
